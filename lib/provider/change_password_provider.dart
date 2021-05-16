74fe5dd18997a0463cf260e3dd854dcb936645e3
karthik <karthik@modsitabox.com>
2021-05-16 10:12:18 +0530

    - change password toast message and back home screen.

--- lib/provider/change_password_provider.dart
+++ lib/provider/change_password_provider.dart
@@ -1,6 +1,8 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:stack_finance_assignment/base_state.dart';
+import 'package:stack_finance_assignment/util/color/colors.dart';
 
 class ChangePasswordProvider extends BaseState {
   /// new password text controller
@@ -20,8 +22,18 @@ class ChangePasswordProvider extends BaseState {
     if (formKey.currentState.validate()) {
       FirebaseAuth.instance.currentUser
           .updatePassword(newPasswordController.text.trim())
-          .then((value) {})
-          .catchError((err) {
+          .then((value) {
+        Fluttertoast.showToast(
+            msg: "Password updated successfully",
+            toastLength: Toast.LENGTH_LONG,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            backgroundColor: Colors.grey,
+            textColor: primaryColor,
+            fontSize: 16.0);
+        Navigator.pop(scaffoldKey.currentContext);
+        Navigator.pop(scaffoldKey.currentContext);
+      }).catchError((err) {
         updateErrorWidget(err);
         debugPrint('password updated error --- $err');
       });
